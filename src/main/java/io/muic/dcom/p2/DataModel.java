941420212ab3fcaa5436056614698461a0866d76
NOK <nokkanato@gmail.com>
2016-11-17 17:47:05 +0700

    first

--- src/main/java/io/muic/dcom/p2/DataModel.java
+++ src/main/java/io/muic/dcom/p2/DataModel.java
@@ -1,7 +1,9 @@
 package io.muic.dcom.p2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 public class DataModel {
@@ -9,6 +11,7 @@ public class DataModel {
         private String parcelId;
         private String stationId;
         private long timeStamp;
+
 
         ParcelObserved(String parcelId_, String stationId_, long ts_) {
             this.parcelId = parcelId_;
@@ -19,28 +22,57 @@ public class DataModel {
         public String getParcelId() { return parcelId; }
         public String getStationId() { return stationId; }
         public long getTimeStamp() { return timeStamp; }
+
     }
 
-    private List<ParcelObserved> transactions;
+    private ConcurrentHashMap<String, ArrayList<ParcelObserved>> hashTrail = new ConcurrentHashMap<>();
+
+    private ConcurrentHashMap<String, Long> hashCount = new ConcurrentHashMap<>();
+    private Long count ;
 
     DataModel() {
-        transactions = new ArrayList<>();
+//        hashTrail = new ConcurrentHashMap<>();
+//        hashCount = new ConcurrentHashMap<>();
+//        count = 1L;
+
+
+
     }
 
-    public synchronized void postObserve(String parcelId, String stationId, long timestamp) {
+    /// IMPLEMENTS CONSTRUCTOR //
+    public void postObserve(String parcelId, String stationId, long timestamp) {
+
         ParcelObserved parcelObserved = new ParcelObserved(parcelId, stationId, timestamp);
-        transactions.add(parcelObserved);
+
+        if (!hashTrail.containsKey(parcelId)){
+            ArrayList<ParcelObserved> b = new ArrayList<>(Arrays.asList(parcelObserved));
+            hashTrail.put(parcelId, b);
+        }else {
+            hashTrail.get(parcelId).add(parcelObserved);
+        }
+
+        if (!hashCount.containsKey(stationId)){
+            hashCount.put(stationId, 1L);
+        }else{
+            long counter = hashCount.get(stationId);
+            counter += 1L;
+            hashCount.put(stationId, counter);
+
+        }
+
+    }
+    // Display only that parceID  <parceID, that station>
+    public ArrayList<ParcelObserved> getParcelTrail(String parcelId) {
+
+        if (!hashTrail.containsKey(parcelId)) {
+            return new ArrayList<>();
+        }else{
+        return hashTrail.get(parcelId);
+        }
     }
 
-    public synchronized List<ParcelObserved> getParcelTrail(String parcelId) {
-        return transactions.stream()
-                .filter(observeEvent -> observeEvent.parcelId.equals(parcelId))
-                .collect(Collectors.toList());
-    }
-
-    public synchronized long getStopCount(String stationId) {
-        return transactions.stream()
-                .filter(observeEvent -> observeEvent.stationId.equals(stationId))
-                .count();
+    // Count only if that station ID < StationID , howmany (Count) >
+    public long getStopCount(String stationId) {
+        return hashCount.get(stationId);
     }
 }
